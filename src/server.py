7cc34a3855ea6097bad7ec4b40027c7ecb00854a
Satish Basetty <satish.basetty@lacity.org>
2021-04-21 16:38:29 -0700

    added the dockerfile build

---
+++ src/server.py
@@ -0,0 +1,21 @@
+import http.server
+import socketserver
+import os
+
+PORT = os.environ.get('PORT',8080)
+
+class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        with open('index.html' ,'rb') as f:
+            self.send_response(200)
+            self.send_header('Content-type','text/html; charset=uft-8')
+            self.end_headers()
+            self.wfile.write(f.read())
+
+
+try:
+    httpd = socketserver.TCPServer(("",int(PORT)), CustomRequestHandler)
+    print("Python webserver listening at port {} ".format(PORT))
+    httpd.serve_forever()
+except Exception as err:
+    print ("Http Exception ")
