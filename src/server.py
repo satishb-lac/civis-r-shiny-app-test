638406cd8f02c02e02a86d91afe40e9aa470499d
Satish Basetty <satish.basetty@lacity.org>
2021-04-21 17:11:57 -0700

    commit

--- src/server.py
+++ src/server.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 import os
 
-PORT = os.environ.get('PORT',8080)
+PORT = os.environ.get('PORT',8081)
 
 class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
